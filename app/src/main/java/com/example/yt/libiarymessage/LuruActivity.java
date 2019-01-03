09802a7059e49d06c78288cfef315d44f54e7a3a
zjx1711605034 <1096208568@qq.com>
2019-01-03 09:31:37 +0800

    添加界面

---
+++ app/src/main/java/com/example/yt/libiarymessage/LuruActivity.java
@@ -0,0 +1,13 @@
+package com.example.yt.libiarymessage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LuruActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_luru);
+    }
+}
