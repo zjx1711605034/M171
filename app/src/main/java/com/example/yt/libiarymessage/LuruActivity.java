6bc64a9fa4f749ee02f53c6e1cb1279f204e3c9c
1711605010 <568874381@qq.com>
2019-01-04 15:41:24 +0800

    修改与添加

--- app/src/main/java/com/example/yt/libiarymessage/LuruActivity.java
+++ app/src/main/java/com/example/yt/libiarymessage/LuruActivity.java
@@ -1,40 +1,87 @@
 package com.example.yt.libiarymessage;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.ConditionVariable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class LuruActivity extends AppCompatActivity {
-    Button btnReturn,btnAdd,btnchange,btndelete;
-    EditText edtID,edtname,edtweizhi;
+public class LuruActivity extends AppCompatActivity implements View.OnClickListener{
+   private Button btnReturn,btnAdd,btnchange,btndelete;
+    private EditText edtID,edtname,edtweizhi;
+    MyDBHelper myDBHelper;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_luru);
-        btnReturn=findViewById(R.id.btn_luru_return);
-        btnAdd=findViewById(R.id.btn_luru_add);
-        btnchange=findViewById(R.id.btn_luru_change);
-        btndelete=findViewById(R.id.btn_luru_delete);
-        edtID=findViewById(R.id.edt_luru_ID);
-        edtname=findViewById(R.id.edt_luru_name);
-        edtweizhi=findViewById(R.id.edt_luru_weizhi);
-        btnReturn.setOnClickListener(new View.OnClickListener() {
+        myDBHelper=new MyDBHelper(this);
+        init();
+    }
+        private void init() {
+            btnReturn = findViewById(R.id.btn_luru_return);
+            btnAdd = findViewById(R.id.btn_luru_add);
+            btnchange = findViewById(R.id.btn_luru_change);
+            btndelete = findViewById(R.id.btn_luru_delete);
+            edtID = findViewById(R.id.edt_luru_ID);
+            edtname = findViewById(R.id.edt_luru_name);
+            edtweizhi = findViewById(R.id.edt_luru_weizhi);
+            btnReturn.setOnClickListener(this);
+            btnAdd.setOnClickListener(this);
+            btnchange.setOnClickListener(this);
+            btndelete.setOnClickListener(this);
+        }
+
             @Override
-            public void onClick(View v) {
+            public void onClick(View v){
                 String ID;
                 String name;
                 String weizhi;
-                ConditionVariable variable;
+                SQLiteDatabase db;
+                ContentValues values;
+                switch (v.getId()) {
+                    case R.id.btn_luru_add:
+                        ID = edtID.getText().toString();
+                        name = edtname.getText().toString();
+                        weizhi = edtweizhi.getText().toString();
+                        db = myDBHelper.getWritableDatabase();
+                        values = new ContentValues();
+                        values.put("ID", ID);
+                        values.put("name", name);
+                        values.put("weizhi", weizhi);
+                        db.insert("information", null, values);
+                        Toast.makeText(this, "录入成功", Toast.LENGTH_SHORT).show();
+                        db.close();
+                        break;
 
-                Intent intent=new Intent(LuruActivity.this,MainActivity.class);
-                startActivity(intent);
+                    case  R.id.btn_luru_change:
+                        db=myDBHelper.getWritableDatabase();
+                        values=new ContentValues();
+                        values.put("ID",ID=edtID.getText().toString());
+                        values.put("name",name=edtname.getText().toString());
+                        values.put("weizhi",weizhi=edtweizhi.getText().toString());
+                        db.update("information",values,"name=?",new String[]{
+                                edtname.getText().toString()});
+                        Toast.makeText(this,"修改成功",Toast.LENGTH_SHORT).show();
+                        db.close();
+                        break;
+
+                    case R.id.btn_luru_delete:
+                        db =myDBHelper.getWritableDatabase();
+                        db.delete("information",null,null);
+                        Toast.makeText(this,"信息已删除",Toast.LENGTH_SHORT).show();
+
+                }
+
+
+
 
             }
-        });
-    }
+
 }
--- app/src/main/java/com/example/yt/libiarymessage/MyDBHelper.java
+++ app/src/main/java/com/example/yt/libiarymessage/MyDBHelper.java
@@ -5,24 +5,16 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class MyDBHelper extends SQLiteOpenHelper {
-    public static final String CREATE_USERDATA="create table userData(" +
-            "id integer primary key autoincrement,"
-            +"nametext,"
-            +"password text)";
-
-    private Context mcontext;
-    public MyDBHelper(Context context,String name,SQLiteDatabase.CursorFactory cursorFactory,int version){
-        super(context,name,cursorFactory,version);
-        mcontext=context;
+   public MyDBHelper(Context context){
+       super(context,"itcast.db",null,1);
     }
-
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(CREATE_USERDATA);
+        db.execSQL("CREATE TABLE information(_id INTEGER PRIMARY KET AUTOINCREMENT,name VARCHAR(20),phone VARCHAR(20))");
     }
-
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    public void onUpgrade(SQLiteDatabase db,int oldVersion,int newVersion){
 
     }
+
 }
