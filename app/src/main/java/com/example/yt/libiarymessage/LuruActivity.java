fa7bab4c1b275f5e7bd8376952f0ddc7ffbeed06
1711605010 <568874381@qq.com>
2019-01-04 11:28:59 +0800

    修改与添加

--- app/src/main/java/com/example/yt/libiarymessage/LuruActivity.java
+++ app/src/main/java/com/example/yt/libiarymessage/LuruActivity.java
@@ -1,24 +1,39 @@
 package com.example.yt.libiarymessage;
 
 import android.content.Intent;
+import android.os.ConditionVariable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class LuruActivity extends AppCompatActivity {
-    Button btnReturn;
+    Button btnReturn,btnAdd,btnchange,btndelete;
+    EditText edtID,edtname,edtweizhi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_luru);
         btnReturn=findViewById(R.id.btn_luru_return);
+        btnAdd=findViewById(R.id.btn_luru_add);
+        btnchange=findViewById(R.id.btn_luru_change);
+        btndelete=findViewById(R.id.btn_luru_delete);
+        edtID=findViewById(R.id.edt_luru_ID);
+        edtname=findViewById(R.id.edt_luru_name);
+        edtweizhi=findViewById(R.id.edt_luru_weizhi);
         btnReturn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String ID;
+                String name;
+                String weizhi;
+                ConditionVariable variable;
+
                 Intent intent=new Intent(LuruActivity.this,MainActivity.class);
                 startActivity(intent);
+
             }
         });
     }
--- app/src/main/java/com/example/yt/libiarymessage/TushucxActivity.java
+++ app/src/main/java/com/example/yt/libiarymessage/TushucxActivity.java
@@ -1,22 +1,35 @@
 package com.example.yt.libiarymessage;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.ConditionVariable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.sql.SQLData;
+
 public class TushucxActivity extends AppCompatActivity {
-    Button btnReturn;
+    Button btnReturn,btncheck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tushucx);
         btnReturn=findViewById(R.id.btn_tushu_return);
+        btncheck=findViewById(R.id.btn_tushu_check);
         btnReturn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                    String ID;
+                    String name;
+                SQLiteDatabase db;
+                ConditionVariable variable;
+                switch (v.getId()){
+                    case R.id.btn_tushu_check:
+
+                }
                 Intent intent=new Intent(TushucxActivity.this,MainActivity.class);
                 startActivity(intent);
             }
