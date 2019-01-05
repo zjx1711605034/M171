e7b7526558dd9325fcecdbf8e8b81dfc70611b64
zjx1711605034 <1096208568@qq.com>
2019-01-05 17:15:40 +0800

    录入与查询

--- app/src/main/java/com/example/yt/libiarymessage/LuruActivity.java
+++ app/src/main/java/com/example/yt/libiarymessage/LuruActivity.java
@@ -1,35 +1,136 @@
 package com.example.yt.libiarymessage;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class LuruActivity extends AppCompatActivity {
-    Button btnReturn;
-        private Toolbar toolbar;
+import com.example.yt.libiarymessage.EntryBeans.Book;
+import com.example.yt.libiarymessage.Server.FunctionServer;
+import com.example.yt.libiarymessage.Server.FunctionServerImp;
+import com.example.yt.libiarymessage.Until.MyUntil;
+
+import java.util.List;
+
+public class LuruActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private Toolbar MyToobar;
+    private TextView tv_luru_title;
+    private TextView tv_luru_ID;
+    private TextView tv_luru_name;
+    private TextView tv_luru_weizhi;
+    private EditText edt_luru_ID;
+    private EditText edt_luru_name;
+    private EditText edt_luru_weizhi;
+    private Button btn_luru_add;
+    private Button btn_luru_change;
+    private Button btn_luru_delete;
+    private Button btn_luru_return;
+    private FunctionServer functionServer;
+    private Activity activity;
+    private Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_luru);
-        btnReturn=findViewById(R.id.btn_luru_return);
-        toolbar=findViewById(R.id.MyToobar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onBackPressed();
-            }
-        });
-        btnReturn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(LuruActivity.this,MainActivity.class);
-                startActivity(intent);
-            }
-        });
+        initView();
+
+
     }
+
+    private void initView() {
+        activity=this;
+        context=this;
+        MyToobar = (Toolbar) findViewById(R.id.MyToobar);
+        tv_luru_title = (TextView) findViewById(R.id.tv_luru_title);
+        tv_luru_ID = (TextView) findViewById(R.id.tv_luru_ID);
+        tv_luru_name = (TextView) findViewById(R.id.tv_luru_name);
+        tv_luru_weizhi = (TextView) findViewById(R.id.tv_luru_weizhi);
+        edt_luru_ID = (EditText) findViewById(R.id.edt_luru_ID);
+        edt_luru_name = (EditText) findViewById(R.id.edt_luru_name);
+        edt_luru_weizhi = (EditText) findViewById(R.id.edt_luru_weizhi);
+        btn_luru_add = (Button) findViewById(R.id.btn_luru_add);
+        btn_luru_change = (Button) findViewById(R.id.btn_luru_change);
+        btn_luru_delete = (Button) findViewById(R.id.btn_luru_delete);
+        btn_luru_return = (Button) findViewById(R.id.btn_luru_return);
+        functionServer=new FunctionServerImp(activity,context);
+        btn_luru_add.setOnClickListener(this);
+        btn_luru_change.setOnClickListener(this);
+        btn_luru_delete.setOnClickListener(this);
+        btn_luru_return.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_luru_add:
+                Addbt();
+                break;
+            case R.id.btn_luru_change:
+                changbt();
+                break;
+            case R.id.btn_luru_delete:
+                deletebt();
+
+                break;
+            case R.id.btn_luru_return:
+                onBackPressed();
+                returnbt();
+
+                break;
+        }
+    }
+
+    //返回按钮
+    private void returnbt() {
+
+    }
+
+    //删除按钮
+    private void deletebt() {
+        try {
+            functionServer.BookDelete(Integer.valueOf(edt_luru_ID.getText()+""),false);
+        }catch (Exception e){
+            Toast.makeText(this,e.toString(),Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //修改按钮
+    private void changbt() {
+        Book entry=null;
+        try {
+             entry = MyUntil.isEntry(Book.class, edt_luru_ID, edt_luru_name, edt_luru_weizhi);
+            functionServer.BookChange(entry,false);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(this,e.toString(),Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    //添加按钮
+    private void Addbt() {
+
+        Book entry =null;
+        try {
+             entry = MyUntil.isEntry(Book.class, edt_luru_ID, edt_luru_name, edt_luru_weizhi);
+                functionServer.BookLuru(entry,false);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(this,e.toString(),Toast.LENGTH_SHORT).show();
+        }
+
+        // Book book=new Book(Integer.valueOf(entry.get(0)),entry.get(1),entry.get(2));
+       // functionServer.BookLuru();
+    }
+
+
 }
--- app/src/main/java/com/example/yt/libiarymessage/TushucxActivity.java
+++ app/src/main/java/com/example/yt/libiarymessage/TushucxActivity.java
@@ -1,36 +1,107 @@
 package com.example.yt.libiarymessage;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.yt.libiarymessage.Adapter.MyBaseAdapter;
+import com.example.yt.libiarymessage.EntryBeans.Book;
+import com.example.yt.libiarymessage.Server.FunctionServer;
+import com.example.yt.libiarymessage.Server.FunctionServerImp;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class TushucxActivity extends AppCompatActivity {
-    Button btnReturn;
-        private Toolbar toolbar;
+public class TushucxActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Toolbar MyToobar;
+    private TextView tv_tushucx;
+    private TextView tv_mian_ID;
+    private TextView tv_main_name;
+    private EditText edt_main_ID;
+    private EditText edt_main_name;
+    private TextView tv_main_show;
+    private Button btn_tushu_check;
+    private Button btn_tushu_return;
+    private FunctionServer functionServer;
+    private Activity activity;
+    private Context context;
+    private ListView Mylist;
+    private MyBaseAdapter myBaseAdapter ;
+    private  List<Book> mybook=new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tushucx);
-        btnReturn=findViewById(R.id.btn_tushu_return);
-        toolbar=findViewById(R.id.MyToobar);
-        setSupportActionBar(toolbar);
+        initView();
+
+    }
+
+    private void initView() {
+        myBaseAdapter=new MyBaseAdapter(mybook,this);
+        activity = this;
+        context = this;
+        functionServer = new FunctionServerImp(activity, context);
+        MyToobar = (Toolbar) findViewById(R.id.MyToobar);
+        tv_tushucx = (TextView) findViewById(R.id.tv_tushucx);
+        tv_mian_ID = (TextView) findViewById(R.id.tv_mian_ID);
+        tv_main_name = (TextView) findViewById(R.id.tv_main_name);
+        edt_main_ID = (EditText) findViewById(R.id.edt_main_ID);
+        edt_main_name = (EditText) findViewById(R.id.edt_main_name);
+        btn_tushu_check = (Button) findViewById(R.id.btn_tushu_check);
+        btn_tushu_return = (Button) findViewById(R.id.btn_tushu_return);
+        setSupportActionBar(MyToobar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+        MyToobar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onBackPressed();
             }
         });
-        btnReturn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(TushucxActivity.this,MainActivity.class);
-                startActivity(intent);
-            }
-        });
+        btn_tushu_check.setOnClickListener(this);
+        btn_tushu_return.setOnClickListener(this);
+        Mylist = (ListView) findViewById(R.id.Mylist);
+
+
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_tushu_check:
+                try {
+                    String ID = edt_main_ID.getText() + "";
+                    String Name = edt_main_name.getText() + "";
+                    Integer IDS = null;
+                    if (ID.equals("")) {
+                        IDS = null;
+                    } else {
+                        IDS = Integer.valueOf(ID);
+                    }
+                    if (Name.equals("")) {
+                        Name = null;
+                    }
+                    Book entry = new Book(IDS, Name, null);
+                    List<Book> books = functionServer.BookChaxun(entry, false);
+                    myBaseAdapter=new MyBaseAdapter(books,this);
+                    myBaseAdapter.notifyDataSetChanged();
+                    Mylist.setAdapter(myBaseAdapter);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                break;
+            case R.id.btn_tushu_return:
+            onBackPressed();
+                break;
+        }
+    }
+
 }
